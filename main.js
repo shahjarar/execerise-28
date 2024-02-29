8f4019b8dd5bb1ecd427162d9c910e73093cf569
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:14:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//Task 29 you really like bananas
+var favourite_fruits = ["kivi", "orange", "apples", "berry", "peasch"];
+if ("kivi") {
+    console.log("kivi");
+}
+if ("peasch") {
+    console.log("you really like bananas");
+}
+if ("orange") {
+    console.log("orange");
+}
+if ("berry") {
+    console.log("you really like bananas");
+}
+if ("apple") {
+    console.log("apple");
+}
---
+++ main.ts
@@ -0,0 +1,26 @@
+//Task 29 you really like bananas
+
+let favourite_fruits : string [] = ["kivi", "orange" ,"apples","berry" ,"peasch"]
+
+if("kivi"){
+    console.log("kivi")
+}
+
+
+if ("peasch"){
+    console.log("you really like bananas")
+}
+
+
+if("orange"){
+    console.log("orange")
+}
+
+
+if("berry"){
+    console.log("you really like bananas")
+}
+
+if ("apple"){
+    console.log("apple")
+}
